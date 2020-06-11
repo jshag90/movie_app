a1528a5895708f3d9e32fe2d97c3d3aaac068798
jshag90 <jshag90@naver.com>
2020-06-11 19:56:06 +0900

    Updates

---
+++ precache-manifest.d6e753af717bdb41f5d27bc3f6e1937a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c915b554ecbeee16fa756b74b3a6f45e",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "8e6d1fc92fb6db05ab9c",
+    "url": "/movie_app/static/css/main.ff010fce.chunk.css"
+  },
+  {
+    "revision": "78a8c6d4641937053d35",
+    "url": "/movie_app/static/js/2.19735fbb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app/static/js/2.19735fbb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e6d1fc92fb6db05ab9c",
+    "url": "/movie_app/static/js/main.194b5b45.chunk.js"
+  },
+  {
+    "revision": "793037b19926e1970ea2",
+    "url": "/movie_app/static/js/runtime-main.8469f341.js"
+  }
+]);
